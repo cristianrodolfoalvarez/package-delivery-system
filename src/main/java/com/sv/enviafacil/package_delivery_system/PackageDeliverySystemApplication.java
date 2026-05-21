430c52bd4f160b101cc5c5a8535f9f8c66cddc4b
Cristian Alvarez <74282863+cristianrodolfoalvarez@users.noreply.github.com>
2026-05-21 10:06:36 -0600

    Incompatibilidades corregidas en pom.xml

--- src/main/java/com/sv/enviafacil/package_delivery_system/PackageDeliverySystemApplication.java
+++ src/main/java/com/sv/enviafacil/package_delivery_system/PackageDeliverySystemApplication.java
@@ -2,8 +2,9 @@ package com.sv.enviafacil.package_delivery_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PackageDeliverySystemApplication {
 
 	public static void main(String[] args) {
