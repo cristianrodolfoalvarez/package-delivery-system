1e6bf88238b0df32b0f2899cb973fc0a5b1c6cb8
Wilber Alvarado <handi_a@hotmail.com>
2026-06-12 23:52:31 -0600

    Actualizacion de formularios y navegacion EnviaFacil

--- frontend/src/app/pages/asignacion-ruta/asignacion-ruta.ts
+++ frontend/src/app/pages/asignacion-ruta/asignacion-ruta.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-asignacion-ruta',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './asignacion-ruta.html',
   styleUrl: './asignacion-ruta.css'
 })
-export class AsignacionRuta {}
+export class AsignacionRuta {
+
+}
